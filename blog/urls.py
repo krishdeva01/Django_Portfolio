b1abf2942af12f53bf73880b450a88e2753e2e31
karthi deva <kris42581@gmail.com>
2021-06-23 22:31:02 +0530

    commiting

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from blog import views
+
+app_name = 'blog'
+
+urlpatterns = [
+ 
+     path('',views.all_blog,name='all_blog'),
+     path('<int:blog_id>/',views.detail,name='detail'),
+]
