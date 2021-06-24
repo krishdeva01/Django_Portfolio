a46eeb9762d414beaf32158bc63cf8a2c8d1fb5c
karthi deva <kris42581@gmail.com>
2021-06-24 15:52:17 +0530

    adding requirements

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -128,7 +128,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     print("looks like no local file.You must be on production")
 
